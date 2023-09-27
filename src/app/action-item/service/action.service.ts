f82f3ff51147e50b9d6f98a9fcc23de7a7ed34f8
Bharat <Bharat@ikcontech.com>
2023-09-27 16:23:31 +0530

    updated action Items URL's

--- src/app/action-item/service/action.service.ts
+++ src/app/action-item/service/action.service.ts
@@ -12,53 +12,45 @@ export class ActionService {
 
     //private gatewayUrl1 = 'http://localhost:8080/api'
     private gatewayUrl ='http://localhost:8012'
-    private actionItemsMicroservicePathUrl = '/actions';
-    private tasksURL ='task';
-    private finalHttpUrl = this.gatewayUrl+this.actionItemsMicroservicePathUrl;  
-    private actionItemsUrl = 'get-actions';
-    private updateActionItemUrl='update-action';
-    private getActionItem = 'get-action-item';
-    private saveActionItemUrl = 'create';
-    private deleteActionItemUrl = 'delete-action';
-    private deleteSelectedAction='ac-items/delete';
+    private actionItemsMicroservicePathUrl = 'actions';
+    private taskMicroServicePathUrl ='task'
     private getTaskDetails ='get-tasks';
-    private getUserSpecificActionItems='fetch-actions';
-
+   
     constructor(private http:HttpClient){}
 
     getAllActionItems(){
-        return this.http.get<ActionItems[]>(`${this.finalHttpUrl}/${this.actionItemsUrl}`,{observe:'response'});
+        return this.http.get<ActionItems[]>(`${this.gatewayUrl}/${this.actionItemsMicroservicePathUrl}/all`,{observe:'response'});
     }
     getActionItemById(id : number){
-      return this.http.get<ActionItems>(`${this.finalHttpUrl}/${this.getActionItem}/${id}`,{observe:'response'});
+      return this.http.get<ActionItems>(`${this.gatewayUrl}/${this.actionItemsMicroservicePathUrl}/${id}`,{observe:'response'});
 
     }
     updateActionItem(actionItem:ActionItems){
       
-       return this.http.put(`${this.finalHttpUrl}/${this.updateActionItemUrl}/${actionItem.actionItemId}`,actionItem,{observe:'response'});
+       return this.http.put(`${this.gatewayUrl}/${this.actionItemsMicroservicePathUrl}/update/${actionItem.actionItemId}`,actionItem,{observe:'response'});
     }
     saveActionItem(actionItem:ActionItems){
       
-      return this.http.post<ActionItems>(`${this.finalHttpUrl}/${this.saveActionItemUrl}`,actionItem,{observe:'response'});
+      return this.http.post<ActionItems>(`${this.gatewayUrl}${this.actionItemsMicroservicePathUrl}/save`,actionItem,{observe:'response'});
        
     }
     getUserActionItems(email:string){
-       return this.http.get<ActionItems[]>(`${this.finalHttpUrl}/${this.getUserSpecificActionItems}/${email}`,{observe:'response'});
+       return this.http.get<ActionItems[]>(`${this.gatewayUrl}/${this.actionItemsMicroservicePathUrl}/all/${email}`,{observe:'response'});
     }
     getAlltaskDetails(){
         
-      return this.http.get<Task[]>(`${this.gatewayUrl}/${this.tasksURL}/${this.getTaskDetails}`,{observe:'response'});
+      return this.http.get<Task[]>(`${this.gatewayUrl}/${this.taskMicroServicePathUrl}/all`,{observe:'response'});
    }
     deleteActionItem(id:number){
 
 
       //console.log(action.id);
-      return this.http.delete<number>(`${this.finalHttpUrl}/${this.deleteActionItemUrl}/${id}`,{observe:'response'});
+      return this.http.delete<number>(`${this.gatewayUrl}/${this.actionItemsMicroservicePathUrl}/${id}`,{observe:'response'});
     }
     
-    deleteSelectedActionItems(actionItems:any[]){
+    deleteSelectedActionItems(actionItemsIds:any[]){
       
-      return this.http.delete<boolean>(`${this.finalHttpUrl}/${this.deleteSelectedAction}/${actionItems}`,{observe:'response'});
+      return this.http.delete<boolean>(`${this.gatewayUrl}/${this.actionItemsMicroservicePathUrl}/${actionItemsIds}`,{observe:'response'});
 
     }
   }
