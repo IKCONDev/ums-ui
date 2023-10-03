1f4c79d022003a37af76265f9956c7a2f18cfda0
Prabhakaran A <prabhakaran.a@ikcontech.com>
2023-10-03 15:06:37 +0530

    updated reports

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -20,9 +20,9 @@ export class ReportComponent implements OnInit {
          res=>{
           this.batchDetails = res.body;
           console.log(this.batchDetails);
-
+          
      });
-      
+ 
 
    }
 
