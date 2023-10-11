59071a18d84ddc2fa30c054919a733ecc74dd62a
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-10-11 20:02:29 +0530

    departments completed

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -55,45 +55,46 @@ export class DepartmentComponent implements OnInit {
   /**
    * create a new department
    */
-  savedDepartment: Department;
+  createdDepartment: Department;
   createDepartment(){
+    let isNameValid = true;
+    let isHeadValid = true;
+    let isCodeValid = true;
+    let isLocationValid = true;
     var flag = 0;
-    if(this.addDepartment.departmentName === ''){
+    
+    if(this.isDepartmentNameValid === false){
+      var valid = this.validateDepartmentName();
+      isNameValid = valid;
       flag = 1;
-      this.departmentNameErrorInfo = 'Department name is required'
-    }else{
-      this.departmentNameErrorInfo = ''
     }
-    if(this.addDepartment.departmentCode === ''){
+    if(this.isDepartmentCodeValid === false){
+     var valid = this.validateDepartmentCode();
+      isCodeValid = valid;
       flag = 1;
-      this.departmentCodeErrorInfo = 'Department code is required'
-    }else{
-      this.departmentCodeErrorInfo = ''
     }
-    if(this.addDepartment.departmentHead === ''){
+    if(this.isDepartmentHeadValid === false){
+     var valid = this.validateDepartmentHead();
+     isHeadValid = valid;
+     flag = 1;
+    }
+    if(this.isDepartmentLocationValid === false){
+      var valid = this.validateDepartmentLocation();
+      isLocationValid = valid;
       flag = 1;
-      this.departmentHeadErrorInfo = 'Department head is required'
-    }else{
-      this.departmentHeadErrorInfo = ''
     }
-    if(this.addDepartment.departmentAddress === ''){
-      flag = 1;
-      this.departmentLocationErrorInfo = 'Department location is required'
-    }else{
-      this.departmentLocationErrorInfo = ''
-    }
-    if(flag==1){
-      this.toastr.error('Please fill required fields')
-    }
-    if(this.departmentNameErrorInfo === '' && this.departmentCodeErrorInfo === ''
-     &&this.departmentHeadErrorInfo === '' && this.departmentLocationErrorInfo=== ''){
+    // if(flag==1){
+    //   this.toastr.error('Please fill the required fields')
+    // }
+    if(isNameValid === true && isCodeValid === true
+     &&isHeadValid === true && isLocationValid === true){
     //set createdBy
     this.addDepartment.createdBy = localStorage.getItem('firstName')+' '+localStorage.getItem('lastName');
     this.addDepartment.createdByEmailId = localStorage.getItem('email');
     this.departmentService.saveDepartment(this.addDepartment).subscribe(
       (response)=> {
         if(response.status === HttpStatusCode.Created){
-          this.savedDepartment = response.body;
+          this.createdDepartment = response.body;
         this.toastr.success('Department added successfully.')
           setTimeout(()=>{
             window.location.reload();
@@ -102,6 +103,85 @@ export class DepartmentComponent implements OnInit {
       }
     )
      }
+  }
+
+  //validations for addDepartment
+  /**
+   * 
+   */
+  departmentNameErrorInfo: string = ''
+  isDepartmentNameValid = false;
+  validateDepartmentName(){
+   // var deptName=  event.target.value;
+    if(this.addDepartment.departmentName === ''){
+      this.departmentNameErrorInfo = 'Department name is required';
+    }else if(this.addDepartment.departmentName.length < 3){
+      this.departmentNameErrorInfo = 'department name should have minimum of 3 chars.';
+    }else if(this.addDepartment.departmentName.length>20){
+      this.departmentNameErrorInfo = 'department name should not exceed more than 20 chars';
+    }else{
+      this.isDepartmentNameValid = true;
+      this.departmentNameErrorInfo = '';
+    }
+    return this.isDepartmentNameValid;
+  }
+
+  /**
+   * 
+   */
+  departmentHeadErrorInfo: string = ''
+  isDepartmentHeadValid = false;
+  validateDepartmentHead(){
+    //var deptHead = event.target.value;
+    if(this.addDepartment.departmentHead === ''){
+      this.departmentHeadErrorInfo = 'Department head is required';
+    }else{
+      this.isDepartmentHeadValid = true;
+      this.departmentHeadErrorInfo = '';
+    }
+    return this.isDepartmentHeadValid;
+  }
+
+  /**
+   * 
+   */
+  departmentCodeErrorInfo:string = '';
+  isDepartmentCodeValid = false;
+  validateDepartmentCode(){
+    //var deptCode = event.target.value;
+    if(this.addDepartment.departmentCode === ''){
+      this.departmentCodeErrorInfo = 'Department code is required';
+    }else if(this.addDepartment.departmentCode.length < 2){
+      this.departmentCodeErrorInfo = 'Department code should be minimum of 2 chars.';
+    }else if(this.addDepartment.departmentCode.length > 4){
+      this.departmentCodeErrorInfo = 'Department code should not exceed 4 chars.';
+    }
+    else{
+      this.isDepartmentCodeValid = true;
+      this.departmentCodeErrorInfo = '';
+    }
+    return this.isDepartmentCodeValid;
+  }
+
+  /**
+   * 
+   */
+  departmentLocationErrorInfo: string= '';
+  isDepartmentLocationValid = false;
+  validateDepartmentLocation(){
+   // var deptLocation = event.target.value;
+    if(this.addDepartment.departmentAddress === ''){
+      this.departmentLocationErrorInfo = 'Department location is required';
+    }else if(this.addDepartment.departmentAddress.length < 3){
+      this.departmentLocationErrorInfo = 'Department location should be minimum of 3 chars.';
+    }else if(this.addDepartment.departmentAddress.length > 20){
+      this.departmentLocationErrorInfo = 'Department location should not exceed 20 chars.';
+    }
+    else{
+      this.isDepartmentLocationValid = true;
+      this.departmentLocationErrorInfo = '';
+    }
+    return this.isDepartmentLocationValid;
   }
 
   /**
@@ -162,35 +242,36 @@ export class DepartmentComponent implements OnInit {
    */
   modifyDepartment(){
     var flag = 0;
-    if(this.existingDepartment.departmentName === ''){
+    let isNameValid = true;
+    let isHeadValid = true;
+    let isCodeValid = true;
+    let isLocationValid = true;
+    
+    if(this.isUpdatedDepartmentNameValid === false){
+      var valid = this.validateUpdatedDepartmentName();
+      isNameValid = valid;
       flag = 1;
-      this.departmentNameErrorInfo = 'Department name is required'
-    }else{
-      this.departmentNameErrorInfo = ''
     }
-    if(this.existingDepartment.departmentCode === ''){
+    if(this.isUpdatedDepartmentCodeValid === false){
+     var valid = this.validateUpdatedDepartmentCode();
+      isCodeValid = valid;
       flag = 1;
-      this.departmentCodeErrorInfo = 'Department code is required'
-    }else{
-      this.departmentCodeErrorInfo = ''
     }
-    if(this.existingDepartment.departmentHead === ''){
+    if(this.isUpdatedDepartmentHeadValid === false){
+     var valid = this.validateUpdatedDepartmentHead();
+     isHeadValid = valid;
+     flag = 1;
+    }
+    if(this.isUpdatedDepartmentLocationValid === false){
+      var valid = this.validateUpdatedDepartmentLocation();
+      isLocationValid = valid;
       flag = 1;
-      this.departmentHeadErrorInfo = 'Department head is required'
-    }else{
-      this.departmentHeadErrorInfo = ''
     }
-    if(this.existingDepartment.departmentAddress === ''){
-      flag = 1;
-      this.departmentLocationErrorInfo = 'Department location is required'
-    }else{
-      this.departmentLocationErrorInfo = ''
-    }
-    if(flag==1){
-      this.toastr.error('Please fill required fields')
-    }
-    if(this.departmentNameErrorInfo === '' && this.departmentCodeErrorInfo === ''
-     &&this.departmentHeadErrorInfo === '' && this.departmentLocationErrorInfo=== ''){
+    // if(flag===1){
+    //   this.toastr.error('Please fill the required fields')
+    // }
+    if(isNameValid === true && isCodeValid === true
+     &&isHeadValid === true && isLocationValid === true){
       this.existingDepartment.modifiedBy = localStorage.getItem('firstName')+' '+localStorage.getItem('lastName');
     console.log(this.existingDepartment)
     this.departmentService.updateDepartment(this.existingDepartment).subscribe(
@@ -207,6 +288,100 @@ export class DepartmentComponent implements OnInit {
      }
   }
 
+  //validations for update department
+  /**
+   * 
+   */
+  updatedDepartmentNameErrorInfo: string = ''
+  isUpdatedDepartmentNameValid = false;
+  validateUpdatedDepartmentName(){
+    // var deptName=  event.target.value;
+     if(this.existingDepartment.departmentName === ''){
+       this.updatedDepartmentNameErrorInfo = 'Department name is required';
+     }else if(this.existingDepartment.departmentName.length < 3){
+       this.updatedDepartmentNameErrorInfo = 'department name should have minimum of 3 chars.';
+     }else if(this.existingDepartment.departmentName.length>20){
+       this.updatedDepartmentNameErrorInfo = 'department name should not exceed more than 20 chars';
+     }else{
+       this.isDepartmentNameValid = true;
+       this.updatedDepartmentNameErrorInfo = '';
+     }
+     return this.isDepartmentNameValid;
+   }
+
+   /**
+    * 
+    */
+  updatedDepartmentHeadErrorInfo: string = ''
+  isUpdatedDepartmentHeadValid = false;
+  validateUpdatedDepartmentHead(){
+    //var deptHead = event.target.value;
+    if(this.existingDepartment.departmentHead === ''){
+      this.updatedDepartmentHeadErrorInfo = 'Department head is required';
+    }else{
+      this.isDepartmentHeadValid = true;
+      this.updatedDepartmentHeadErrorInfo = '';
+    }
+    return this.isDepartmentHeadValid;
+  }
+
+  /**
+   * 
+   */
+  updatedDepartmentCodeErrorInfo: string = ''
+  isUpdatedDepartmentCodeValid = false;
+  validateUpdatedDepartmentCode(){
+    //var deptCode = event.target.value;
+    if(this.existingDepartment.departmentCode === ''){
+      this.updatedDepartmentCodeErrorInfo = 'Department code is required';
+    }else if(this.existingDepartment.departmentCode.length < 2){
+      this.updatedDepartmentCodeErrorInfo = 'Department code should be minimum of 2 chars.';
+    }else if(this.existingDepartment.departmentCode.length > 4){
+      this.updatedDepartmentCodeErrorInfo = 'Department code should not exceed 4 chars.';
+    }
+    else{
+      this.isDepartmentCodeValid = true;
+      this.updatedDepartmentCodeErrorInfo = '';
+    }
+    return this.isDepartmentCodeValid;
+  }
+
+  /**
+   * 
+   */
+  updatedDepartmentLocationErrorInfo: string = ''
+  isUpdatedDepartmentLocationValid = false;
+  validateUpdatedDepartmentLocation(){
+    // var deptLocation = event.target.value;
+     if(this.existingDepartment.departmentAddress === ''){
+       this.updatedDepartmentLocationErrorInfo = 'Department location is required';
+     }else if(this.existingDepartment.departmentAddress.length < 3){
+       this.updatedDepartmentLocationErrorInfo = 'Department location should be minimum of 3 chars.';
+     }else if(this.existingDepartment.departmentAddress.length > 20){
+       this.updatedDepartmentLocationErrorInfo = 'Department location should not exceed 20 chars.';
+     }
+     else{
+       this.isDepartmentLocationValid = true;
+       this.updatedDepartmentLocationErrorInfo = '';
+     }
+     return this.isDepartmentLocationValid;
+   }
+
+   /**
+    * 
+    */
+   clearErrorMessages(){
+    this.departmentNameErrorInfo = '';
+    this.departmentCodeErrorInfo = '';
+    this.departmentHeadErrorInfo = '';
+    this.departmentLocationErrorInfo = '';
+
+    this.updatedDepartmentNameErrorInfo = '';
+    this.updatedDepartmentCodeErrorInfo = '';
+    this.updatedDepartmentHeadErrorInfo = '';
+    this.updatedDepartmentLocationErrorInfo = '';
+  }
+
   /*
   closeDepartmentModal(){
     this.addDepartment.departmentName= '';
@@ -215,79 +390,5 @@ export class DepartmentComponent implements OnInit {
     this.addDepartment.departmentAddress = '';
   }
   */
-
-  //validations for updateDepartment
-  departmentNameErrorInfo: string = ''
-  validateDepartmentName(event: any){
-    var deptName=  event.target.value;
-    if(deptName === ''){
-      this.departmentNameErrorInfo = 'Department name is required';
-    }else if(deptName.length < 3){
-      this.departmentNameErrorInfo = 'department name should have minimum of 3 chars.';
-    }else if(deptName.length>20){
-      this.departmentNameErrorInfo = 'department name should not exceed more than 20 chars';
-    }else{
-      this.departmentNameErrorInfo = '';
-    }
-  }
-
-  departmentHeadErrorInfo: string = ''
-  validateDepartmentHead(event: any){
-    var deptHead = event.target.value;
-    if(deptHead === ''){
-      this.departmentHeadErrorInfo = 'Department head is required';
-    }else{
-      this.departmentHeadErrorInfo = '';
-    }
-  }
-
-  departmentCodeErrorInfo:string = '';
-  validateDepartmentCode(event: any){
-    var deptCode = event.target.value;
-    if(deptCode === ''){
-      this.departmentCodeErrorInfo = 'Department code is required';
-    }else if(deptCode.length < 2){
-      this.departmentCodeErrorInfo = 'Department code should be minimum of 2 chars.';
-    }else if(deptCode.length > 4){
-      this.departmentCodeErrorInfo = 'Department code should not exceed 4 chars.';
-    }
-    else{
-      this.departmentCodeErrorInfo = '';
-    }
-  }
-
-  departmentLocationErrorInfo: string= '';
-  validateDepartmentLocation(event: any){
-    var deptLocation = event.target.value;
-    if(deptLocation === ''){
-      this.departmentLocationErrorInfo = 'Department location is required';
-    }else if(deptLocation.length < 3){
-      this.departmentLocationErrorInfo = 'Department location should be minimum of 3 chars.';
-    }else if(deptLocation.length > 20){
-      this.departmentLocationErrorInfo = 'Department location should not exceed 20 chars.';
-    }
-    else{
-      this.departmentLocationErrorInfo = '';
-    }
-  }
-
-  // //validations for add department
-  // validateDepartmentName(event: any){
-  //   if(event.target.value === ''){
-  //     //this.existingDepartmentNameErrorInfo = ''
-  //   }
-  // }
-
-  // validateDepartmentHead(){
-
-  // }
-
-  // validateDepartmentCode(){
-
-  // }
-
-  // validateDepartmentLocation(){
-
-  // }
 
 }
