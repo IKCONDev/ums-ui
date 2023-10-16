a58c991368680ed8ba0eda13c5ed2acd440cb6b1
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-10-16 15:37:50 +0530

    added role component add, update, delete, view

--- src/app/model/Role.model.ts
+++ src/app/model/Role.model.ts
@@ -1,5 +1,18 @@
-export interface Role{
+export class Role extends Object{
     roleId:number;
     roleName: string;
+    createdBy: string;
+    createdByEmailId: string;
+    createdDateTime : Date;
+    modifiedBy: string;
+    modifiedByEmailId: string;
+    modifiedDateTime: Date;
+
+    /**
+     *
+     */
+    constructor() {
+        super();
+    }
 
 }
