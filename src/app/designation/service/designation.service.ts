8f710f4fe021885b77ae49374475d5c5d0a83fa4
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-10-19 15:37:05 +0530

    resolved checkbox bugs

--- src/app/designation/service/designation.service.ts
+++ src/app/designation/service/designation.service.ts
@@ -1,13 +1,13 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Department } from "src/app/model/Department.model";
 import { Designation } from "src/app/model/Designation.model";
 
 @Injectable({
-    providedIn : 'root'
+    providedIn: 'root'
 })
 
-export class DesignationService{
+export class DesignationService {
 
     gatewayMicroservicePathUrl: string = 'http://localhost:8012';
     designationMicroservicepathUrl: string = 'designations';
@@ -16,16 +16,21 @@ export class DesignationService{
      * 
      * @param http 
      */
-    constructor(private http: HttpClient){
-        
+    constructor(private http: HttpClient) {
+
     }
 
     /**
      * 
      * @returns 
      */
-    getDesignationList(){
-        return this.http.get<Designation[]>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/all`,{observe:'response'});
+    getDesignationList() {
+        return this.http.get<Designation[]>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/all`, {
+            observe: 'response', headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+            }
+            )
+        });
     }
 
     /**
@@ -33,9 +38,14 @@ export class DesignationService{
      * @param designation 
      * @returns 
      */
-    createDesignation(designation: any){
+    createDesignation(designation: any) {
         console.log(designation)
-        return this.http.post<Designation>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/create`,designation,{observe:'response'});
+        return this.http.post<Designation>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/create`, designation, {
+            observe: 'response', headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+            }
+            )
+        });
     }
 
     /**
@@ -43,8 +53,13 @@ export class DesignationService{
      * @param designationId 
      * @returns 
      */
-    deleteDesignation(designationId: number){
-        return this.http.delete<boolean>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/delete/${designationId}`,{observe:'response'});
+    deleteDesignation(designationId: number) {
+        return this.http.delete<boolean>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/delete/${designationId}`, {
+            observe: 'response', headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+            }
+            )
+        });
     }
 
     /**
@@ -52,8 +67,13 @@ export class DesignationService{
      * @param updatedDesignation 
      * @returns 
      */
-    updateDesignation(updatedDesignation: any){
-        return this.http.put<Designation>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/update`,updatedDesignation,{observe:'response'});
+    updateDesignation(updatedDesignation: any) {
+        return this.http.put<Designation>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/update`, updatedDesignation, {
+            observe: 'response', headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+            }
+            )
+        });
     }
 
     /**
@@ -61,13 +81,23 @@ export class DesignationService{
      * @param designationId 
      * @returns 
      */
-    getDesignation(designationId: number){
+    getDesignation(designationId: number) {
         console.log(designationId)
-        return this.http.get<any>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/get/${designationId}`,{observe:'response'});
+        return this.http.get<any>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/get/${designationId}`, {
+            observe: 'response', headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+            }
+            )
+        });
     }
 
-    deleteAllSelectedDesignationsById(ids: any[]){
-        return this.http.delete<boolean>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/delete/all/${ids}`,{observe:'response'});
+    deleteAllSelectedDesignationsById(ids: any[]) {
+        return this.http.delete<boolean>(`${this.gatewayMicroservicePathUrl}/${this.designationMicroservicepathUrl}/delete/all/${ids}`, {
+            observe: 'response', headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')
+            }
+            )
+        });
     }
 
 }
