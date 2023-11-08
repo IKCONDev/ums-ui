65cdc2541a58067d9afa546607842e0053ca956c
Dhanush Akunuri <dhanush.akunuri@ikcontech.com>
2023-11-08 14:54:04 +0530

    resolved validation bug of reporting manager in employee module

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -459,7 +459,7 @@ export class EmployeeComponent implements OnInit, OnDestroy, AfterViewInit {
   employeeReportingManagerErrorInfo = "";
   validateEmployeeReportingManager() {
 
-    if (this.addEmployee.reportingManager == '') {
+    if (this.addEmployee.reportingManager == '' || this.addEmployee.reportingManager===null) {
       this.employeeReportingManagerErrorInfo = "Reporting Manager is required";
       this.isEmployeeReportingManagerValid = false;
     }
