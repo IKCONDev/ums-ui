063c032e0a130d4cbc6460d13a01aaef3f3353e2
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-11-16 16:48:08 +0530

    latest

--- src/app/meetings/meetings.component.ts
+++ src/app/meetings/meetings.component.ts
@@ -31,7 +31,7 @@ export class MeetingsComponent implements OnInit, OnDestroy, AfterViewInit {
   eventId: number;
 
   //global varibales
-  meetings: Meeting[];
+  meetings: Meeting[]= [];
   meetingCount: number = 0;
   attendedMeetings: Meeting[];
   attendedMeetingCount: number = 0;
@@ -194,7 +194,6 @@ export class MeetingsComponent implements OnInit, OnDestroy, AfterViewInit {
    * executes when the component is initialized or loaded first time
    */
   ngOnInit(): void {
-
     this.getActiveUMSAttendeesEmailIdList();
     //get user data on switch reportee or user
     this.headerService.fetchUserProfile(this.selectedReporteeOrganizedMeeting!=''?this.selectedReporteeOrganizedMeeting:this.loggedInUser).subscribe({
@@ -224,6 +223,10 @@ export class MeetingsComponent implements OnInit, OnDestroy, AfterViewInit {
     this.tabOpened = localStorage.getItem('tabOpened')
     console.log(this.tabOpened)
     this.getMeetings(this.tabOpened);
+
+    setTimeout(() => {
+      this.enableOrDisableMOMEmailButton();
+    },1000)
 
     //disable actionItem btn default
     this.isActionItemSaveButtonDisabled = true;
@@ -1679,6 +1682,24 @@ export class MeetingsComponent implements OnInit, OnDestroy, AfterViewInit {
       })
       this.closeFilterModal();
       window.location.reload();
+  }
+
+  /**
+   * 
+   */
+  enableOrDisableMOMEmailButton(){
+    this.meetings.map(meeting => {
+      var table = document.getElementById('myTable'+meeting.meetingId);
+      console.log('myTable'+meeting.meetingId)
+      var cells = table.getElementsByTagName('td');
+      console.log(cells)
+      if(cells.length > 0){
+        console.log(true)
+        console.log('email'+meeting.meetingId)
+        var momBTN = $('#email'+meeting.meetingId)
+        momBTN.attr("disabled","disabled")
+      }
+    }) 
   }
 
 }
