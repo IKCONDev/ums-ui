6d001bbf712448601c3170c02056adc88daa9dfd
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-11-26 14:38:13 +0530

    added action item reports

--- src/app/action-item-reports/action-item-reports.component.ts
+++ src/app/action-item-reports/action-item-reports.component.ts
@@ -2,9 +2,9 @@ import { Component, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-action-items-reports',
-  templateUrl: './action-items-reports.component.html',
-  styleUrls: ['./action-items-reports.component.css']
+  selector: 'app-action-item-reports',
+  templateUrl: './action-item-reports.component.html',
+  styleUrls: ['./action-item-reports.component.css']
 })
 export class ActionItemsReportsComponent {
   reportType: string;
