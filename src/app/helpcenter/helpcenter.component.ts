37b266571f278da3d9afa1cd1cbefa5537c845d4
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-12-05 17:30:00 +0530

    added permissions for settings, reports and helpcenter

--- src/app/helpcenter/helpcenter.component.ts
+++ src/app/helpcenter/helpcenter.component.ts
@@ -1,11 +1,84 @@
-import { Component,Output } from '@angular/core';
+import { Component,OnInit,Output } from '@angular/core';
+import { MenuItem } from '../model/MenuItem.model';
+import { lastValueFrom } from 'rxjs';
+import { Router } from '@angular/router';
+import { AppMenuItemService } from '../app-menu-item/service/app-menu-item.service';
+import { HttpStatusCode } from '@angular/common/http';
 
 @Component({
   selector: 'app-helpcenter',
   templateUrl: './helpcenter.component.html',
   styleUrls: ['./helpcenter.component.css']
 })
-export class HelpcenterComponent {
+export class HelpcenterComponent implements OnInit {
+  
   @Output() title: string = 'Help Center';
+  viewPermission: boolean;
+  createPermission: boolean;
+  updatePermission: boolean;
+  deletePermission: boolean;
+  userRoleMenuItemsPermissionMap: Map<string, string>
+
+  constructor(private router: Router, private menuItemService: AppMenuItemService){}
+
+ async ngOnInit(): Promise<void> {
+    if(localStorage.getItem('jwtToken') === null){
+      this.router.navigateByUrl('/session-timeout');
+    }
+    
+    if (localStorage.getItem('userRoleMenuItemPermissionMap') != null) {
+      this.userRoleMenuItemsPermissionMap = new Map(Object.entries(JSON.parse(localStorage.getItem('userRoleMenuItemPermissionMap'))));
+    }
+    //get menu item  details of home page
+    var currentMenuItem = await this.getCurrentMenuItemDetails();
+    console.log(currentMenuItem)
+
+      if (this.userRoleMenuItemsPermissionMap.has(currentMenuItem.menuItemId.toString().trim())) {
+        //provide permission to access this component for the logged in user if view permission exists
+        console.log('exe')
+        //get permissions of this component for the user
+        var menuItemPermissions = this.userRoleMenuItemsPermissionMap.get(this.currentMenuItem.menuItemId.toString().trim());
+        if (menuItemPermissions.includes('View')) {
+          this.viewPermission = true;
+        }else{
+          this.viewPermission = false;
+        }
+        if (menuItemPermissions.includes('Create')) {
+          this.createPermission = true;
+        }else{
+          this.createPermission = false;
+        }
+        if (menuItemPermissions.includes('Update')) {
+          this.updatePermission = true;
+        }else{
+          this.updatePermission = false;
+        }
+        if (menuItemPermissions.includes('Delete')) {
+          this.deletePermission = true;
+        }else{
+          this.deletePermission = false;
+        }
+      }
+  }
+
+  currentMenuItem: MenuItem;
+  async getCurrentMenuItemDetails() : Promise<MenuItem> {
+      const response =  await lastValueFrom(this.menuItemService.findMenuItemByName('Help Center')).then(response => {
+        if (response.status === HttpStatusCode.Ok) {
+          this.currentMenuItem = response.body;
+          console.log(this.currentMenuItem)
+        }else if(response.status === HttpStatusCode.Unauthorized){
+          console.log('eit')
+          this.router.navigateByUrl('/session-timeout');
+        }
+      },reason => {
+        if(reason.status === HttpStatusCode.Unauthorized){
+          this.router.navigateByUrl('/session-timeout')
+        }
+      }
+      )
+    console.log(this.currentMenuItem);
+    return this.currentMenuItem;
+  }
 
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from '../model/MenuItem.model';
+import { lastValueFrom } from 'rxjs';
+import { AppMenuItemService } from '../app-menu-item/service/app-menu-item.service';
+import { HttpStatusCode } from '@angular/common/http';
 
 @Component({
   selector: 'app-settings',
@@ -15,19 +19,80 @@ export class SettingsComponent implements OnInit {
   /**
    * 
    */
-  constructor(private router: Router){
+  constructor(private router: Router, private menuItemService: AppMenuItemService){
     
   }
 
   /**
    * 
    */
-  ngOnInit(): void {
+  userRoleMenuItemsPermissionMap: Map<string, string>;
+  viewPermission: boolean;
+  createPermission: boolean;
+  updatePermission: boolean;
+  deletePermission: boolean;
+
+  async ngOnInit(): Promise<void> {
     if(this.loggedInUserRole != 'ADMIN' && this.loggedInUserRole != 'SUPER_ADMIN'){
       this.router.navigateByUrl('/unauthorized');
     }
+    if(localStorage.getItem('jwtToken') === null){
+      this.router.navigateByUrl('/session-timeout');
+    }
+    
+    if (localStorage.getItem('userRoleMenuItemPermissionMap') != null) {
+      this.userRoleMenuItemsPermissionMap = new Map(Object.entries(JSON.parse(localStorage.getItem('userRoleMenuItemPermissionMap'))));
+    }
+    //get menu item  details of home page
+    var currentMenuItem = await this.getCurrentMenuItemDetails();
+    console.log(currentMenuItem)
+
+      if (this.userRoleMenuItemsPermissionMap.has(currentMenuItem.menuItemId.toString().trim())) {
+        //provide permission to access this component for the logged in user if view permission exists
+        console.log('exe')
+        //get permissions of this component for the user
+        var menuItemPermissions = this.userRoleMenuItemsPermissionMap.get(this.currentMenuItem.menuItemId.toString().trim());
+        if (menuItemPermissions.includes('View')) {
+          this.viewPermission = true;
+        }else{
+          this.viewPermission = false;
+        }
+        if (menuItemPermissions.includes('Create')) {
+          this.createPermission = true;
+        }else{
+          this.createPermission = false;
+        }
+        if (menuItemPermissions.includes('Update')) {
+          this.updatePermission = true;
+        }else{
+          this.updatePermission = false;
+        }
+        if (menuItemPermissions.includes('Delete')) {
+          this.deletePermission = true;
+        }else{
+          this.deletePermission = false;
+        }
+      }
   }
 
-
+  currentMenuItem: MenuItem;
+  async getCurrentMenuItemDetails() : Promise<MenuItem> {
+      const response =  await lastValueFrom(this.menuItemService.findMenuItemByName('Settings')).then(response => {
+        if (response.status === HttpStatusCode.Ok) {
+          this.currentMenuItem = response.body;
+          console.log(this.currentMenuItem)
+        }else if(response.status === HttpStatusCode.Unauthorized){
+          console.log('eit')
+          this.router.navigateByUrl('/session-timeout');
+        }
+      },reason => {
+        if(reason.status === HttpStatusCode.Unauthorized){
+          this.router.navigateByUrl('/session-timeout')
+        }
+      }
+      )
+    console.log(this.currentMenuItem);
+    return this.currentMenuItem;
+  }
 
 }
