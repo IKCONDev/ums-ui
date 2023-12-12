17d2d6f85870dede3b4d881ccb4acd973ded47ab
Bharat <Bharat@ikcontech.com>
2023-12-12 13:21:39 +0530

    updated

--- src/app/forgot-password-email-verification/forgot-password-email-verification.component.ts
+++ src/app/forgot-password-email-verification/forgot-password-email-verification.component.ts
@@ -140,7 +140,7 @@ export class ForgotPasswordEmailVerificationComponent {
     )
   }
   else{
-    this.toastr.error("Email ID is incorrect , OTP has not sent ")
+    this.toastr.error("Email ID is incorrect , OTP has not been sent ")
   }
 
       
