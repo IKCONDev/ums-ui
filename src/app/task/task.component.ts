7cf94abfc9e4da1624f372ead3071ab01bc87b2a
Prabhakaran A <prabhakaran.a@ikcontech.com>
2023-12-15 11:58:04 +0530

    latest

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -115,7 +115,7 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
           paging: true,
           searching: true,
           pageLength: 10,
-          orderFixed: [[0, 'asc']],
+          order: [[0, 'asc']],
           lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]], // Set the options for the "Show entries" dropdown
           // Add other options here as needed
         });
@@ -142,8 +142,9 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
           paging: true,
           searching: true, // Enable search feature
           pageLength: 10,
-          order: [[1, 'asc']],
+          order: [[0, 'asc']],
           lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+          
           // Add other options here as needed
         });
       });
