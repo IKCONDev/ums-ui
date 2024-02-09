9acc36b9919707501563468fa8155577bb4a855f
Prabhakaran A <prabhakaran.a@ikcontech.com>
2024-02-09 11:59:25 +0530

    latest

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -951,7 +951,7 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
 
     }
     else {
-      this.toastr.warning("Task " + id + " is not deleted.");
+     // this.toastr.warning("Task " + id + " is not deleted.");
 
     }
 
