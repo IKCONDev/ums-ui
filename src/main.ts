95262b5665fca1e064ebbc812bcbe1ab3a79e90e
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2024-02-19 11:54:13 +0530

    resolved null issue in title of application

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,6 @@ import { AppModule } from './app/app.module';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-  var firstName = localStorage.getItem('firstName')
-  var lastName = localStorage.getItem('lastName');
-  if (firstName != null && lastName != null) {
-    document.title = "UMS - " + firstName + " " + lastName;
-  }
-  else {
-    document.title = "UMS";
-  }
-})
-  .catch(err => console.error(err))
-
-
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
 
