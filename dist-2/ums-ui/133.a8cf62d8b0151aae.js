1ef27be035c63604fe8e3bfc1779f16044211167
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2024-03-07 14:23:39 +0530

    latest

--- dist-2/ums-ui/133.a8cf62d8b0151aae.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkums_ui=self.webpackChunkums_ui||[]).push([[133],{7133:(i,u,t)=>{t.r(u),t.d(u,{LazyModule:()=>c});var n=t(6814),a=t(4769);let c=(()=>{class s{static#t=this.\u0275fac=function(o){return new(o||s)};static#s=this.\u0275mod=a.oAB({type:s});static#u=this.\u0275inj=a.cJS({imports:[n.ez]})}return s})()}}]);
