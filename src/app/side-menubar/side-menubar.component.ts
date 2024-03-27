49bd3721ada7e6a4c0609d6ae09c210e5f99a03b
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2024-03-27 21:13:50 +0530

    latest

--- src/app/side-menubar/side-menubar.component.ts
+++ src/app/side-menubar/side-menubar.component.ts
@@ -3,6 +3,10 @@ import { style } from '@angular/animations';
 import { ElementRef, Output,OnInit } from '@angular/core';
 import { Component, EventEmitter, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from '../model/MenuItem.model';
+import { lastValueFrom } from 'rxjs';
+import { HttpStatusCode } from '@angular/common/http';
+import { AppMenuItemService } from '../app-menu-item/service/app-menu-item.service';
 
 @Component({
   selector: 'app-side-menubar',
@@ -15,17 +19,63 @@ export class SideMenubarComponent {
   activate='';
   isMenuActive:boolean = false;
   overviewActive:string;
+  settingsViewPermission: string;
 
-  constructor(private router: Router){
-
+  constructor(private router: Router, private menuItemService: AppMenuItemService){
+    this.settingsViewPermission = localStorage.getItem('settingsViewPermission');
   }
 
-
+  userRoleMenuItemsPermissionMap: Map<string, string>;
+  viewPermission: boolean;
+  createPermission: boolean;
+  updatePermission: boolean;
+  deletePermission: boolean;
+  noPermissions: boolean;
   /**
    * 
    */
-  ngOninit(){
+  async ngOnInit(): Promise<void> {
+    // if(this.loggedInUserRole != 'ADMIN' && this.loggedInUserRole != 'SUPER_ADMIN'){
+    //   this.router.navigateByUrl('/unauthorized');
+    // }
+    if(localStorage.getItem('jwtToken') === null){
+      this.router.navigateByUrl('/session-timeout');
+    }
     
+    if (localStorage.getItem('userRoleMenuItemPermissionMap') != null) {
+      this.userRoleMenuItemsPermissionMap = new Map(Object.entries(JSON.parse(localStorage.getItem('userRoleMenuItemPermissionMap'))));
+    }
+    //get menu item  details of home page
+    var currentMenuItem = await this.getCurrentMenuItemDetails();
+      if (this.userRoleMenuItemsPermissionMap.has(currentMenuItem.menuItemId.toString().trim())) {
+        //this.noPermissions = false;
+        //provide permission to access this component for the logged in user if view permission exists
+        //get permissions of this component for the user
+        var menuItemPermissions = this.userRoleMenuItemsPermissionMap.get(this.currentMenuItem.menuItemId.toString().trim());
+        if (menuItemPermissions.includes('View')) {
+          this.viewPermission = true;
+        }else{
+          this.viewPermission = false;
+        }
+        if (menuItemPermissions.includes('Create')) {
+          this.createPermission = true;
+        }else{
+          this.createPermission = false;
+        }
+        if (menuItemPermissions.includes('Update')) {
+          this.updatePermission = true;
+        }else{
+          this.updatePermission = false;
+        }
+        if (menuItemPermissions.includes('Delete')) {
+          this.deletePermission = true;
+        }else{
+          this.deletePermission = false;
+        }
+      }else{
+       // this.noPermissions = true;
+       this.router.navigateByUrl('/unauthorized');
+      }
   }
 
   /**
@@ -39,6 +89,23 @@ export class SideMenubarComponent {
       this.activate='active';
     }
     return this.isMenuActive
+  }
+
+  currentMenuItem: MenuItem;
+  async getCurrentMenuItemDetails() : Promise<MenuItem> {
+      const response =  await lastValueFrom(this.menuItemService.findMenuItemByName('Settings')).then(response => {
+        if (response.status === HttpStatusCode.Ok) {
+          this.currentMenuItem = response.body;
+        }else if(response.status === HttpStatusCode.Unauthorized){
+          this.router.navigateByUrl('/session-timeout');
+        }
+      },reason => {
+        if(reason.status === HttpStatusCode.Unauthorized){
+          this.router.navigateByUrl('/session-timeout')
+        }
+      }
+      )
+    return this.currentMenuItem;
   }
 }
 
